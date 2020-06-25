4b7856f8a6c0d80dfeaf449d6a5cff90aac80b7e
Jackie_Hsu <421977279@qq.com>
2020-06-25 17:25:38 +0800

    响应拦截器配置

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import axios from 'axios';
 import 'vant/lib/index.css';
 
 Vue.use(Vant);
+
+import { Toast } from 'vant';
+
 // 绑定到原型上
 Vue.prototype.$axios = axios;
 
@@ -17,19 +20,39 @@ router.beforeEach((to, from, next) => {
   console.log(to);
 
   // 路由守卫进行登录权限校验
-  if(to.meta.auth){
+  if (to.meta.auth) {
 
     const userToken = localStorage.getItem("userToken");
 
-    if(userToken){
+    if (userToken) {
       return next();
-    }else{
+    } else {
       return router.push('/login')
     }
   }
 
   // 路由守卫需要放行
   next();
+
+})
+
+// 拦截器配置
+axios.interceptors.response.use(res => {
+  const {
+    statusCode,
+    message
+  } = res.data;
+
+  if (statusCode && statusCode === 401) {
+    Toast.fail('用户信息校验失败，重新登录');
+
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userToken');
+
+    router.replace('/login')
+  }
+
+  return res;
 
 })
 
